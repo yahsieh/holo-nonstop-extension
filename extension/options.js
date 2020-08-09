5c81062461fdba21cc91f88d7408526f4e65ad14
Brian Hsieh <yahsieh@cs.nctu.edu.tw>
2020-08-10 02:08:33 +0800

    Implement load function

--- extension/options.js
+++ extension/options.js
@@ -1,15 +1,5 @@
 /* eslint-disable no-undef */
 // Saves options to chrome.storage
-
-$('#my-select').multiSelect({ keepOrder: true })
-
-$('#deselect-all').click(function () {
-    $('#my-select').multiSelect('deselect_all')
-    $('#my-select').multiSelect('refresh')
-})
-
-
-
 function save_options() {
     let newList = []
     selectedCount = $('.ms-elem-selection[style=""] > span').length
@@ -17,7 +7,7 @@ function save_options() {
         newList.push($('.ms-elem-selection[style=""] > span')[index].textContent)
     }
     chrome.storage.sync.set({
-        hostlist: newList
+        hostOrder: newList
     }, function () {
         // Update status to let user know options were saved.
         let status = document.getElementById('status');
@@ -28,17 +18,37 @@ function save_options() {
     });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-// function restore_options() {
-//     // Use default value color = 'red' and likesColor = true.
-//     chrome.storage.sync.get({
-//         favoriteColor: 'red',
-//         likesColor: true
-//     }, function(items) {
-//         document.getElementById('color').value = items.favoriteColor;
-//         document.getElementById('like').checked = items.likesColor;
-//     });
-// }
-// document.addEventListener('DOMContentLoaded', restore_options);
+function init() {
+    $('#my-select').multiSelect({ keepOrder: true })
+    restore_options()
+}
+function restore_options() {
+    // Use default value hostOrder = []
+    chrome.storage.sync.get({
+        hostOrder: []
+    }, function(items) {
+        $('#my-select').multiSelect('deselect_all')
+        $('#my-select').multiSelect('refresh')
+        items.hostOrder.forEach(items => {
+            $('#my-select').multiSelect('select', getChannelId(items))
+        });
+    });
+}
+
+function getChannelId(channelName) {
+    let convertList = {
+        'elem 1': 'elem_1',
+        'elem 2': 'elem_2',
+        'elem 3': 'elem_3',
+        'elem 4': 'elem_4'
+    }
+    return convertList[channelName]
+}
+
+$('#deselect-all').click(function () {
+    $('#my-select').multiSelect('deselect_all')
+    $('#my-select').multiSelect('refresh')
+})
+document.addEventListener('DOMContentLoaded', init);
 document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('load').addEventListener('click', restore_options);
